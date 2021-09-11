5b0d29a7da4647cf161d4d91abc553ddc5d4387b
Orlando Ding <xiandao.airs@gmail.com>
2021-09-11 23:10:00 +0800

    orlando - for updates of loadsym and test

---
+++ test/loadsym.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <dlfcn.h>
+
+int main() {
+    void* nvmlhandle = dlopen("./build/lib/libnvidia-ml.so.1", RTLD_NOW);
+    if (!nvmlhandle) {
+        printf("error for loading library\n");
+        return -1;
+    }
+    const char* symbol = "nvmlDeviceGetCudaComputeCapability";
+    void* tmp = dlsym(nvmlhandle, symbol);                         
+    if (tmp == NULL) {                                   
+      printf("dlsym failed on %s - %s\n", symbol, dlerror());
+      return -1;
+    } 
+    return 0;
+}
